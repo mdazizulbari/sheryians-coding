b3ad595f8a57480bfa57dec3ed9918d548c53cdc
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-06 12:12:14 +0600

    constructor functions

---
+++ javascript/index.js
@@ -0,0 +1,27 @@
+// higher order function
+// the functions which accpects a function in a parameter or return a function or both
+// 1st way
+function abcd(value){}
+abcd(function(){})
+// 2nd way
+function bcde(){
+    return function(){}
+}
+bcde()
+// forEach always takes another function inside it, so it's a higher order function
+var arr = [1,2,3,4,5]
+arr.forEach(function(){})
+
+
+
+// constructor functions
+// normal function where you use "this" and use "new" while calling the function
+// used when you want to make something where most of the properties are same, without some exceptions
+function makeCircularButton(color){
+    this.radius = 2
+    this.color = color
+    this.icon = false
+    this.pressable = true
+}
+var redBtn = new makeCircularButton(`red`)
+var blueBtn = new makeCircularButton(`blue`)
