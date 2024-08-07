93d02e0929122a6ce5f1f72602c4320c1a345bfa
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-07 22:24:21 +0600

    playing the song while changing main image

--- 04.level-2/music-player/index.js
+++ 04.level-2/music-player/index.js
@@ -20,3 +20,31 @@ var arr = [
         image: "/04.level-2/music-player/images/its-only-woth-it.jpg",
     },
 ]
+
+var allSongs = document.querySelector('#all-songs')
+var selectedSongImage = document.querySelector('#left')
+var audio = new Audio()
+var selectedSong = 0
+
+function songToHTML(){
+    var clutter = ''
+    arr.forEach(function(element, index){
+        clutter += `<div id="${index}" class="song-card flex border-b-2 border-gray-600 items-center justify-between py-4 px-4 rounded-xl hover:bg-gray-600 ">
+                        <div class="part1 flex items-center pointer-events-none">
+                            <img class="w-20 h-20 mr-5 rounded-xl" src="${element.image}" alt="">
+                            <h3 class="text-xl">${element.songName}</h3>
+                        </div>
+                        <h6 class="text-xl">3:45</h6>
+                    </div>`
+    })
+    allSongs.innerHTML = clutter
+    audio.src = arr[selectedSong].url
+    selectedSongImage.style.backgroundImage = `url(${arr[selectedSong].image})`
+}
+
+songToHTML()
+allSongs.addEventListener('click', function(details){
+    selectedSong = details.target.id
+    songToHTML()
+    audio.play()
+})
