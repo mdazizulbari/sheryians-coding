5ac13a78557cdba31982d78b6725633de6995ed8
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-07 21:14:20 +0600

    data entry

---
+++ 04.level-2/music-player/index.js
@@ -0,0 +1,22 @@
+var arr = [
+    {
+        songName: "NEFFEX Failure",
+        url: "/04.level-2/music-player/music/failure.mp3",
+        image: "/04.level-2/music-player/images/failuer.webp",
+    },
+    {
+        songName: "NEFFEX Porpose",
+        url: "/04.level-2/music-player/music/purpose.mp3",
+        image: "/04.level-2/music-player/images/pourpose.jpg",
+    },
+    {
+        songName: "NEFFEX Take It",
+        url: "/04.level-2/music-player/music/take-it.mp3",
+        image: "/04.level-2/music-player/images/take-it.jpg",
+    },
+    {
+        songName: "NEFFEX It's Only Worth It If You Work For It",
+        url: "/04.level-2/music-player/music/its-only-woth-it.mp3",
+        image: "/04.level-2/music-player/images/its-only-woth-it.jpg",
+    },
+]
