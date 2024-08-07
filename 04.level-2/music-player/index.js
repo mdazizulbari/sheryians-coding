71da1599e0c566c7482ba4e1d7375847f4a7a249
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-07 23:23:23 +0600

    made play and pause button functional

--- 04.level-2/music-player/index.js
+++ 04.level-2/music-player/index.js
@@ -3,38 +3,46 @@ var arr = [
         songName: "NEFFEX Failure",
         url: "/04.level-2/music-player/music/failure.mp3",
         image: "/04.level-2/music-player/images/failuer.webp",
+        duration: "07:51",
     },
     {
         songName: "NEFFEX Porpose",
         url: "/04.level-2/music-player/music/purpose.mp3",
         image: "/04.level-2/music-player/images/pourpose.jpg",
+        duration: "03:55",
     },
     {
         songName: "NEFFEX Take It",
         url: "/04.level-2/music-player/music/take-it.mp3",
         image: "/04.level-2/music-player/images/take-it.jpg",
+        duration: "03:17",
     },
     {
         songName: "NEFFEX It's Only Worth It If You Work For It",
         url: "/04.level-2/music-player/music/its-only-woth-it.mp3",
         image: "/04.level-2/music-player/images/its-only-woth-it.jpg",
+        duration: "03:13",
     },
 ]
 
-var allSongs = document.querySelector('#all-songs')
 var selectedSongImage = document.querySelector('#left')
+var allSongs = document.querySelector('#all-songs')
+var backward = document.querySelector('#backward')
+var play = document.querySelector('#play')
+var forward = document.querySelector('#forward')
+
 var audio = new Audio()
 var selectedSong = 0
 
 function songToHTML(){
     var clutter = ''
     arr.forEach(function(element, index){
-        clutter += `<div id="${index}" class="song-card flex border-b-2 border-gray-600 items-center justify-between py-4 px-4 rounded-xl hover:bg-gray-600 ">
+        clutter += `<div id="${index}" class="song-card flex items-center justify-between py-4 px-4 rounded-2xl hover:bg-gray-600 ">
                         <div class="part1 flex items-center pointer-events-none">
                             <img class="w-20 h-20 mr-5 rounded-xl" src="${element.image}" alt="">
                             <h3 class="text-xl">${element.songName}</h3>
                         </div>
-                        <h6 class="text-xl">3:45</h6>
+                        <h6 class="text-xl">${element.duration}</h6>
                     </div>`
     })
     allSongs.innerHTML = clutter
@@ -47,4 +55,21 @@ allSongs.addEventListener('click', function(details){
     selectedSong = details.target.id
     songToHTML()
     audio.play()
+    play.innerHTML = `<i class="ri-pause-fill"></i>`
+    flag = 1
+})
+
+var flag = 0
+play.addEventListener('click', function(){
+    if(flag == 0){
+        play.innerHTML = `<i class="ri-pause-fill"></i>`
+        songToHTML()
+        audio.play()
+        flag = 1
+    } else{
+        play.innerHTML = `<i class="ri-play-fill"></i>`
+        songToHTML()
+        audio.pause()
+        flag = 0
+    }
 })
