3a2d48487cfc2b922b19c7632fb8102b93ef41ce
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-15 06:33:31 +0600

    started js

---
+++ p3.js/script.js
@@ -0,0 +1,24 @@
+//  1 - 10
+// 20 - 30
+i=12;
+while(i>0){
+    console.log(i)
+    i--
+}
+// 32 - 45
+// 45 - 32
+// 12 - 1
+for(i=1;i<11;i++){console.log(i)}
+for(i=20;i<31;i++){console.log(i)}
+for(i=32;i<46;i++){console.log(i)}
+for(i=12;i>0;i--){console.log(i)}
+
+
+// functions ------------------------------------------
+// es5 & es6
+// es5 - function statements, function expressions, anonymos functions
+// es6 - fat arrow function
+// a) basic fat arro
+// b) fat arrow with one param
+// c) fat arrow with implicit return
+7min
