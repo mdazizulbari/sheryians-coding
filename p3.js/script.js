2d58da84c1eb3ab60ac189c3c2efe109a233ff1b
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-15 21:30:04 +0600

    JS question practice

--- p3.js/script.js
+++ p3.js/script.js
@@ -51,7 +51,7 @@ button2.addEventListener("click", function () {
   img2.src = src1;
 });
 
-var h4 = document.querySelector("h4");
+var h4 = document.getElementById("formh4");
 var inputs = document.querySelectorAll('input[type="text"]');
 var form = document.getElementById("form");
 form.addEventListener("submit", function (event) {
@@ -59,8 +59,90 @@ form.addEventListener("submit", function (event) {
   for (let inputIndex = 0; inputIndex < inputs.length; inputIndex++) {
     if (inputs[inputIndex].value === "") {
       h4.innerText = "Please fill all the boxes";
-      h4.style.color = 'red'
+      h4.style.color = "red";
       break;
     }
   }
 });
+
+var inputItem = document.getElementById("inputItem");
+var addItem = document.getElementById("addItem");
+var removeItem = document.getElementById("removeItem");
+var ul = document.getElementById("ul");
+var li;
+
+addItem.addEventListener("click", function () {
+  if (inputItem.value === "") {
+  } else {
+    li = document.createElement("li");
+    li.textContent = inputItem.value;
+    ul.appendChild(li);
+    inputItem.value = "";
+  }
+});
+removeItem.addEventListener("click", function () {
+  ul.removeChild(li);
+});
+
+
+var counterH4 = document.getElementById('counterH4')
+var counterStart = document.getElementById('counterStart')
+var counterStop = document.getElementById('counterStop')
+var displayCounter;
+
+counterStart.addEventListener('click', function(){
+    displayCounter = 0
+    counter = setInterval(() => {
+        displayCounter++
+        counterH4.innerText = displayCounter
+    }, 1000);
+})
+counterStop.addEventListener('click', function(){
+    clearInterval(counter)
+})
+
+
+
+var About = document.getElementById('About')
+var about = document.getElementById('about')
+var News = document.getElementById('News')
+var news = document.getElementById('news')
+var Home = document.getElementById('Home')
+var home = document.getElementById('home')
+
+Home.addEventListener('click', function(){
+    clearAllInfo()
+    home.style.display = 'block'
+})
+About.addEventListener('click', function(){
+    clearAllInfo()
+    about.style.display = 'block'
+})
+News.addEventListener('click', function(){
+    clearAllInfo()
+    news.style.display = 'block'
+})
+function clearAllInfo(){
+    home.style.display = 'none'
+    news.style.display = 'none'
+    about.style.display = 'none'
+}
+
+
+
+var progressBg = document.getElementById('progressBg')
+var progress = document.getElementById('progress')
+var taskRunning = document.getElementById('taskRunning')
+var taskCompleted = document.getElementById('taskCompleted')
+var taskProgress = 0
+
+taskProcessing = setInterval(() => {
+    taskProgress++
+    if(taskProgress >= 100){
+        clearInterval(taskProcessing)
+        taskRunning.style.display = 'none'
+        taskCompleted.style.display = 'block'
+    }
+    progress.style.width = taskProgress+'%'
+    console.log(taskProgress)
+}, 200);
