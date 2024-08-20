7980a3c8459848a47e2e035f101f6c8589b01475
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-21 00:24:16 +0600

    added adding friends frunctionality

--- p9.react/02.firstPractice/src/App.jsx
+++ p9.react/02.firstPractice/src/App.jsx
@@ -1,47 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Components/Card.jsx";
 
 function App() {
-  const data = [
+  const raw = [
     {
       name: "Sma",
       profession: "student",
       image:
         "https://images.unsplash.com/photo-1519058082700-08a0b56da9b4?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      friends: false,
     },
     {
       name: "Smith",
       profession: "agent",
       image:
         "https://images.unsplash.com/photo-1480455624313-e29b44bbfde1?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      friends: false,
     },
     {
       name: "Abbas",
       profession: "teacher",
       image:
         "https://images.unsplash.com/photo-1484515991647-c5760fcecfc7?q=80&w=1949&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      friends: false,
     },
     {
       name: "Rohan",
       profession: "engineer",
       image:
         "https://images.unsplash.com/photo-1524538198441-241ff79d153b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      friends: false,
     },
     {
       name: "Ivu",
       profession: "doctor",
       image:
         "https://plus.unsplash.com/premium_photo-1674933214600-483da3cb2d0c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      friends: false,
     },
   ];
 
+  const [data, setData] = useState(raw);
+  const handleFriendButton = (cardindex) => {
+    setData((previousState) => {
+      return previousState.map((item, index) => {
+        if (index === cardindex) {
+          return { ...item, friends: !item.friends };
+        }
+        return item;
+      });
+    });
+  };
+
   return (
     <>
-    <div className="w-full h-screen bg-gray-200 flex gap-4 items-center justify-center">
-      {data.map((item, index)=>(
-      <Card values={item} />
-      ))}
-    </div>
+      <div className="w-full h-screen bg-gray-200 flex flex-wrap gap-4 items-center justify-center">
+        {data.map((item, index) => (
+          <Card
+            values={item}
+            key={index}
+            handleFriendButton={handleFriendButton}
+            index={index}
+          />
+        ))}
+      </div>
     </>
   );
 }
--- p9.react/02.firstPractice/src/Components/Card.jsx
+++ p9.react/02.firstPractice/src/Components/Card.jsx
@@ -1,5 +1,5 @@
-const Card = ({values}) => {
-    const {name, profession, image} = values
+const Card = ({ values, handleFriendButton, index }) => {
+  const { name, profession, image, friends } = values;
   return (
     <div className="w-52 rounded-xl overflow-hidden bg-white">
       <div className="w-full h-40 bg-blue-200">
@@ -8,8 +8,11 @@ const Card = ({values}) => {
       <div className="w-full p-3">
         <h3 className="font-semibold text-xl">{name}</h3>
         <h5 className="text-xs">{profession}</h5>
-        <button onClick={()=>alert("Friend Added")} className="px-3 py-1 my-3 text-xs text-white font-semibold rounded-lg bg-blue-600">
-          Add Friend
+        <button
+          onClick={()=>handleFriendButton(index)}
+          className={`px-3 py-1 my-3 text-xs text-white font-semibold rounded-lg ${friends ? "bg-green-600" : "bg-blue-600"}`}
+        >
+          {friends === true ? "Friends" : "Add Friend"}
         </button>
       </div>
     </div>
