a15b10c0673f81652d272f07df08fb918004bc74
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-20 22:45:40 +0600

    got the data into cards

--- p9.react/02.firstPractice/src/App.jsx
+++ p9.react/02.firstPractice/src/App.jsx
@@ -1,23 +1,47 @@
 import React from "react";
-import Card from "./Components/Card";
-import Practice from "./Components/Practice";
-import ImageSlide from "./Components/ImageSlide";
-import PropsLesson from "./Components/PropsLesson";
+import Card from "./Components/Card.jsx";
 
 function App() {
-  // const data = ["SMA", "SMI", "Abbas", "Rohan", "Ivu"];
+  const data = [
+    {
+      name: "Sma",
+      profession: "student",
+      image:
+        "https://images.unsplash.com/photo-1519058082700-08a0b56da9b4?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Smith",
+      profession: "agent",
+      image:
+        "https://images.unsplash.com/photo-1480455624313-e29b44bbfde1?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Abbas",
+      profession: "teacher",
+      image:
+        "https://images.unsplash.com/photo-1484515991647-c5760fcecfc7?q=80&w=1949&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Rohan",
+      profession: "engineer",
+      image:
+        "https://images.unsplash.com/photo-1524538198441-241ff79d153b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Ivu",
+      profession: "doctor",
+      image:
+        "https://plus.unsplash.com/premium_photo-1674933214600-483da3cb2d0c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
+
   return (
     <>
-      <PropsLesson text={"Download"} color={"bg-blue-600"} />
-      <PropsLesson text={"Know More"} color={"bg-red-600"} />
-      {/* <ImageSlide /> */}
-      {/* <Practice /> */}
-      {/* <Card/> */}
-      {/* {data.map((element, index) => (
-        <div key={index} className="px-3 py-4 bg-yellow-400 rounded-md w-fit">
-          {element}
-        </div>
-      ))} */}
+    <div className="w-full h-screen bg-gray-200 flex gap-4 items-center justify-center">
+      {data.map((item, index)=>(
+      <Card image={item.image} name={item.name} profession={item.profession} />
+      ))}
+    </div>
     </>
   );
 }
