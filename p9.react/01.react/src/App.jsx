55a058dce395611ae37940d320477c436a99844c
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-20 13:43:38 +0600

    made the card

--- p9.react/01.react/src/App.jsx
+++ p9.react/01.react/src/App.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import Card from "./Components/Card";
 
 function App() {
+  // const data = ["SMA", "SMI", "Abbas", "Rohan", "Ivu"];
   return (
     <>
-      <Card />
+    <Card/>
+      {/* {data.map((element, index) => (
+        <div key={index} className="px-3 py-4 bg-yellow-400 rounded-md w-fit">
+          {element}
+        </div>
+      ))} */}
     </>
   );
 }
--- p9.react/01.react/src/Components/Card.jsx
+++ p9.react/01.react/src/Components/Card.jsx
@@ -1,21 +1,64 @@
 import React from "react";
 
-function Card(){
-    return (
-<>
-<div className="w-full h-screen bg-gray-200">
-    <div className="w-52 rounded-md bg-gray-100 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-    <div className="w-full h-32 rounded-md bg-gray-300 overflow-hidden">
-        <img className=" w-full h-full object-cover" src="https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-    </div>
-    <div className="w-full px-3 py-4">
-        <h5 className="font-semibold">Amazon Basics</h5>
-            <p className="text-xs mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos ut optio cupiditate!</p>
-    </div>
-    </div>
-</div>
-</>
-    )
+function Card() {
+    const eventAddtoCart = ()=>{alert("Added to cart");}
+  const data = [
+    {
+      name: "Apple Store",
+      description: "Get all your Apple products in one place!",
+      image:
+        "https://images.unsplash.com/photo-1602053540920-027b608ed4db?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      instock: false,
+    },
+    {
+      name: "Amazon Basics",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos ut optio cupiditate!",
+      image:
+        "https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      instock: true,
+    },
+    {
+      name: "Google Pixel 8 Pro",
+      description: "The best Android that you can find in 2024",
+      image:
+        "https://fdn.gsmarena.com/imgroot/reviews/23/google-pixel-8-pro/lifestyle/-1024w2/gsmarena_010.jpg",
+      instock: true,
+    },
+  ];
+  return (
+    <>
+      <div className="w-full h-screen bg-gray-600 flex gap-10 items-center justify-center">
+        {data.map((element, index) => (
+          <div
+            key={index}
+            className="w-52 rounded-xl bg-gray-100 overflow-hidden"
+          >
+            <div className="w-full h-32 bg-gray-300 ">
+              <img
+                className=" w-full h-full object-cover"
+                src={element.image}
+              />
+            </div>
+            <div className="w-full px-3 py-4">
+              <h5 className="font-semibold">{element.name}</h5>
+              <p className="text-xs mt-3">{element.description}</p>
+              <p
+                className={`text-xs ${
+                  element.instock ? "bg-green-600" : "bg-red-600"
+                } px-3 py-1 w-fit rounded-xl mt-3 text-white`}
+              >
+                {element.instock ? "In Stock" : "Out of Stock"}
+              </p>
+              <button onClick={eventAddtoCart}
+                className={`text-xs px-3 py-1 hover:bg-blue-700 ${element.instock ? "block" : "hidden"} bg-blue-600 w-full rounded-xl mt-3 text-white`}
+              >Add to Cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Card;
