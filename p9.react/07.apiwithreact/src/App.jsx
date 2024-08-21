247c9ee3cc8198c19ae716a6051b55b564032184
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-21 20:17:29 +0600

    getting and displaying the product titles

--- p9.react/07.apiwithreact/src/App.jsx
+++ p9.react/07.apiwithreact/src/App.jsx
@@ -1,6 +1,62 @@
+import axios from "axios";
+import { useState } from "react";
+
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const addproducts = () => {
+    const api = "https://fakestoreapi.com/products";
+    axios
+      .post(api, {
+        title: "test product",
+        price: 13.5,
+        description: "lorem ipsum set",
+        image: "https://i.pravatar.cc",
+        category: "electronic",
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => console.log(error));
+  };
+  const getproducts = () => {
+    const api = "https://fakestoreapi.com/products/";
+    axios
+      .get(api)
+      .then((products) => {
+        console.log(products);
+        setProducts(products.data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  console.log(products);
+
   return (
-    <div></div>
-  )
-}
-export default App
+    <div className="w-full h-screen flex flex-col gap-5 justify-center items-center">
+      <button
+        onClick={addproducts}
+        className="px-4 py-2 rounded-full bg-blue-600 text-white"
+      >
+        Add Products In Api
+      </button>
+      <button
+        onClick={getproducts}
+        className="px-4 py-2 rounded-full bg-blue-600 text-white"
+      >
+        Get Products
+      </button>
+      <ul>
+        {products.length > 0 ? (
+          products.map((product) => (
+            <li className="px-4 py-2 mb-1 rounded-full bg-green-600 text-white">
+              {product.title}
+            </li>
+          ))
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </ul>
+    </div>
+  );
+};
+export default App;
