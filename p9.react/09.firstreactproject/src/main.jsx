de9c096269e0982e78656d3d700b84d4bb18e7e5
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-22 12:02:14 +0600

    globalized porducts data

--- p9.react/09.firstreactproject/src/main.jsx
+++ p9.react/09.firstreactproject/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import Context from "./Utilities/Context.jsx";
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
+  <Context>
+  {/* <StrictMode> */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  // </StrictMode>
+   {/* </StrictMode> */}
+  </Context>
 );
