ea42f8a15c3a3034b764dde5d8c03f2dd78aa6ee
sma-bari <mdazizulbarishafin@gmail.com>
2024-08-22 12:20:38 +0600

    displaying the products in homepage

--- p9.react/09.firstreactproject/src/Components/Home.jsx
+++ p9.react/09.firstreactproject/src/Components/Home.jsx
@@ -12,19 +12,21 @@ const Home = () => {
     <>
       <Nav />
       <section className="w-10/12 p-10 flex flex-wrap overflow-x-hidden overflow-y-auto">
-        <Link
-          to="/details/1"
-          className="card w-1/5 h-[30vh] mr-3 mb-3 border shadow rounded-lg flex flex-col justify-center items-center"
-        >
-          <div
-            style={{
-              backgroundImage:
-                "url(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)",
-            }}
-            className="w-full h-4/5 bg-contain bg-no-repeat bg-center hover:scale-110"
-          ></div>
-          <h3>Lorem ipsum dolor sit amet.</h3>
-        </Link>
+        {products.map((product, index) => (
+          <Link
+            to="/details/1"
+            className="card w-1/5 h-[30vh] mr-3 mb-3 border shadow rounded-lg flex flex-col justify-center items-center"
+          >
+            <div
+              style={{
+                backgroundImage:
+                  `url(${product.image})`,
+              }}
+              className="w-full h-4/5 bg-contain bg-no-repeat bg-center hover:scale-110"
+            ></div>
+            <h3>{product.title}</h3>
+          </Link>
+        ))}
       </section>
     </>
   ) : (
