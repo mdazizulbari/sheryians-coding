5680682c7cb61113cae1db150d114ae21831ed0e
smabari <mdazizulbarishafin@gmail.com>
2024-12-01 11:47:39 +0600

    cynchronus api in nodejs

--- p13.backend/script.js
+++ p13.backend/script.js
@@ -1,39 +1,39 @@
 // Creating files
 import { writeFile } from "node:fs";
-const dataForNewFile = "Created a new file using nodeJS";
-writeFile("newFile.txt", dataForNewFile, (err) => {
-  if (err) throw err;
-  console.log("File created successfully!");
-});
+// const dataForNewFile = "Created a new file using nodeJS";
+// writeFile("newFile.txt", dataForNewFile, (err) => {
+//   if (err) throw err;
+//   console.log("File created successfully!");
+// });
 
 // reading files
 import { readFile } from "node:fs";
-readFile("newFile.txt", "utf8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+// readFile("newFile.txt", "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
 
 // appending or adding new data
 import { appendFile } from "node:fs";
-const addingNewData = ". New added data data";
-appendFile("newFile.txt", addingNewData, (err) => {
-  if (err) throw err;
-  console.log("Data appended successfully!");
-});
+// const addingNewData = ". New added data data";
+// appendFile("newFile.txt", addingNewData, (err) => {
+//   if (err) throw err;
+//   console.log("Data appended successfully!");
+// });
 
 // renaming file
 import { rename } from "node:fs";
-rename("newFile.txt", "renamedFile.txt", (err) => {
-  if (err) throw err;
-  console.log("Rename complete!");
-});
+// rename("newFile.txt", "renamedFile.txt", (err) => {
+  // if (err) throw err;
+  // console.log("Rename complete!");
+// });
 
 // deleting file
 import { unlink } from "node:fs";
-unlink("renamedFile.txt", (err) => {
-  if (err) throw err;
-  console.log("File deleted successfully!");
-});
+// unlink("renamedFile.txt", (err) => {
+  // if (err) throw err;
+  // console.log("File deleted successfully!");
+// });
 
 // creating folder
 import { mkdir } from "node:fs";
@@ -55,3 +55,11 @@ import { rm } from "node:fs";
 //     if (err) throw err;
 //     console.log("Folder deleted successfully!");
 // })
+
+// importing all the files system modules
+// const fs = require("fs");
+import fs from "node:fs";
+
+// synchronuss api
+let data = fs.readFileSync("renamedFile.txt");
+console.log("read");
